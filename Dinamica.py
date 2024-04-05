4f9c68bec164d161edf9cdbff1b1936630556f82
Ironwolf1212 <velasco.miguel@correounivalle.edu.co>
2024-04-04 22:38:28 -0500

    Update DynProg algorithm

--- Dinamica.py
+++ Dinamica.py
@@ -3,17 +3,17 @@ import copy
 
 def costoRiego(plantacion, tiempo_transcurrido):
     """
-    Calcula el costo de riego de una plantación en función del tiempo transcurrido.
+    Calcula el costo de riego de una plantaciï¿½n en funciï¿½n del tiempo transcurrido.
 
     Args:
-        plantacion (tuple): Una tupla que contiene información sobre la plantación.
-            - tiempo_supervivencia (int): Tiempo máximo de supervivencia de la plantación sin riego.
-            - tiempo_regado (int): Tiempo necesario para regar la plantación.
-            - prioridad (int): Prioridad de la plantación.
+        plantacion (tuple): Una tupla que contiene informaciï¿½n sobre la plantaciï¿½n.
+            - tiempo_supervivencia (int): Tiempo mï¿½ximo de supervivencia de la plantaciï¿½n sin riego.
+            - tiempo_regado (int): Tiempo necesario para regar la plantaciï¿½n.
+            - prioridad (int): Prioridad de la plantaciï¿½n.
         tiempo_transcurrido (int): Tiempo transcurrido desde el inicio.
 
     Returns:
-        int: El costo de riego de la plantación.
+        int: El costo de riego de la plantaciï¿½n.
 
     """
     tiempo_supervivencia = plantacion.tiempoSuperv
@@ -26,68 +26,133 @@ def costoRiego(plantacion, tiempo_transcurrido):
     
 def ordenOptimoDinamica(finca):
     #Arreglo para almacenar los costos de las soluciones a los subproblemas
-    secuencias = [[]] * len(finca)
+    secuencias = []
+    for i in range(len(finca)):
+        secuencias.append([])
     print(secuencias)
     #Crear los costos de los casos bases (regar cada elemento de la finca en tiempo = 0) 
     for plantacion in finca:
         #print("Costo riego: ",costoRiego(plantacion,0))
         secuencias[0].append([[plantacion],plantacion.tiempoRiego,costoRiego(plantacion, 0)])
     indiceSecuencias = 0
-    print("Secuencias[0]: ",secuencias[0])
-    secuencias[1] = []
-    secuencias[2] = []
-    secuencias[3] = []
-    secuencias[4] = []
-    print("Secuencias[1]: ",secuencias[1])
-    print("Secuencias[0]: ",secuencias[0])
     #Crear los costos de los casos recursivos
     #Que no combine las secuencias con su misma longitud sino con secuencias[0]
     for longitud in secuencias:
-        print("Longitud: ", longitud)
+        #print("Longitud: ", longitud)
         
         ##Calcular los costos para esa longitud
-        #Buscar la forma de no combinarse con los elementos que ya combinó... quizá haciendo uso de las plantaciones que contiene y buscando en secuencias[0]
+        #Buscar la forma de no combinarse con los elementos que ya combinï¿½... quizï¿½ haciendo uso de las plantaciones que contiene y buscando en secuencias[0]
         longitudBackup = copy.copy(longitud)
         
         for secuenciaOptima in longitudBackup:
-            print("Secuencia optima: ",secuenciaOptima)
-            ## hallar el costo de poner este de primero y los demás de segundo, posteriormente expulsar del arreglo para evitar repetir
+            #print("Secuencia optima: ",secuenciaOptima)
+            
+            ## hallar el costo de poner este de primero y los demï¿½s de segundo, posteriormente expulsar del arreglo para evitar repetir
             ## probar orden en momento de hallar costo para escoger el minimo
             ## Guardar el tiempo que llevan acumuladas cada secuencia, calcular todas las secuencias de la siguiente longitud en un arreglo temporal y luego meterlas en costo?? no es necesario
-            ## Guardar cada longitud de secuencia en un arreglo diferente??? podría ser
-            ## Guardar las secuencias de cada longitud en una posición de un arreglo dinámico.
+            ## Guardar cada longitud de secuencia en un arreglo diferente??? podrï¿½a ser
+            ## Guardar las secuencias de cada longitud en una posiciï¿½n de un arreglo dinï¿½mico.
             plantacionesCombinar = copy.copy(secuencias[0])
             for plantaEscogida in secuenciaOptima[0]:
-                print("Planta escogida: ", plantaEscogida)
-                print("Secuencias[0]: ", secuencias[0])
+                #print("Planta escogida: ", plantaEscogida, "Secuencia Optima[0]: ",secuenciaOptima[0])
+                #print("plantacionesCombinar: ", plantacionesCombinar)
+                #print("Secuencias[0]: ", secuencias[0])
                 index = 0
+                Encontrado = False
                 for plantacionOptima in plantacionesCombinar:
-                    print("Planta escogida: ", plantaEscogida)
-                    print("Plantacion optima: ", plantacionOptima)
-                    print("Plantaciones combinar: ", plantacionesCombinar)
-                    print("Indice: ", index)
-                    if plantaEscogida in plantacionOptima[0]:
-                        print("Planta escogida: ", plantaEscogida)
-                        print("Plantacion optima: ", plantacionOptima)
-                        print("Plantaciones combinar: ", plantacionesCombinar)
-                        print("Indice: ", index)
+                    #print("Planta escogida: ", plantaEscogida)
+                    #print("Plantacion optima: ", plantacionOptima)
+                    #print("Plantaciones combinar: ", plantacionesCombinar)
+                    #print("Indice: ", index)
+                    if str(plantaEscogida) == str(plantacionOptima[0][0]):
+                        #print("Planta escogida: ", plantaEscogida)
+                        #print("Plantacion optima: ", plantacionOptima)
+                        #print("Plantaciones combinar: ", plantacionesCombinar)
+                        #print("Indice: ", index)
                         plantacionesCombinar[index] = None
+                        encontrado = True
                     for plantacion in plantacionesCombinar:
                         if plantacion == None:
                             plantacionesCombinar.pop(plantacionesCombinar.index(plantacion))
+                    if Encontrado:
+                        break
                     index += 1
                 #if plantaEscogida in secuencias[0]:
                     
                     #plantacionesCombinar.pop(plantacionesCombinar.index(plantaEscogida))
-            print("plantacionesCombinar: ",plantacionesCombinar)
+            #print("plantacionesCombinar: ",plantacionesCombinar)
             #plantacionesCombinar.remove(secuenciaOptima)
             for nuevaPlantacion in plantacionesCombinar:
-                print("Plantaciones a anadir: ",secuenciaOptima,nuevaPlantacion)
+                #print("Plantaciones a anadir: ",secuenciaOptima,nuevaPlantacion)
+                #Si esta combinacion de plantaciones ya estÃ¡ en la secuencia objetivo, saltar el cÃ¡lculo.
+                secuenciaSaltar = copy.deepcopy(secuenciaOptima)
+                #print("secuencia a saltar: ",secuenciaSaltar,"Secuencia optima: ", secuenciaOptima)
+                secuenciaSaltar[0].append(nuevaPlantacion[0][0])
+                secuenciaSaltar[2] += costoRiego(nuevaPlantacion[0][0], secuenciaSaltar[1])
+                secuenciaSaltar[1] = int(secuenciaSaltar[1])
+                secuenciaSaltar[1] += int(nuevaPlantacion[1])
+                #print("secuencia a saltar: ",secuenciaSaltar,"Secuencia optima: ", secuenciaOptima)
+                #print("secuencias[",indiceSecuencias+1,"]: ", secuencias[indiceSecuencias+1])
+                
+
+                #EstÃ¡ purgando de mÃ¡s, tiene que verificar que la que estÃ¡ es mÃ¡s barata que la que va a entrar? pero si tiene que encontrar match exactos... y las que estÃ¡n
+                #DeberÃ­an ser Ã³ptimas... << sÃ³lo se cumple para los de longitud 2 para abajo, al resto se le tienen alternativas menos Ã³ptimas de manera temporal
+                #PodrÃ­a cambiar a que haga comparaciones aquÃ­ y sÃ³lo inserte las que mejoran, no sÃ© quÃ© tan bueno sea, creo que no es bueno... << Foreshadowing sÃ­ era bueno
+                #A lo mejor estoy haciendo un mal enfoque, en vez de sacar los que tengan las mismas plantaciones, deberÃ­a sacar...
+                #TIENE QUE MIRAR SI HAY UNO EN EL MISMO ORDEN EN EL QUE EL ENTRARIA CLARROOOOOOO FUCK
+
+                seEncontro = False
+                for secuenciaOptimaComparar in secuencias[indiceSecuencias+1]:
+                    if indiceSecuencias == 0:
+                        if secuenciaSaltar[0][0] == secuenciaOptimaComparar[0][0] or secuenciaSaltar[0][0] == secuenciaOptimaComparar[0][1]:
+                            if secuenciaSaltar[0][1] == secuenciaOptimaComparar[0][0] or secuenciaSaltar[0][1] == secuenciaOptimaComparar[0][1]:
+                                seEncontro = True
+                                break
+                    
+                    plantacionesRepetidas = 0
+                    #print("Comparando ",secuenciaSaltar," con ",secuenciaOptimaComparar[0])
+                    #if str(secuenciaSaltar) == str(secuenciaOptimaComparar[0]):
+                    #    seEncontro = True
+                    #   break
+                    for plantacionSaltar in secuenciaSaltar[0]:
+                        #print("Comparando ",plantacionSaltar, " con ",secuenciaOptimaComparar[0])
+                        for plantacionComparar in secuenciaOptimaComparar[0]:
+                            if str(plantacionSaltar) == str(plantacionComparar):
+                                #print("Esta en ",plantacionSaltar, " con ",secuenciaOptimaComparar[0])
+                                plantacionesRepetidas +=1
+                    #print("PlantacionesRepetidas: ",plantacionesRepetidas," de ", len(secuenciaOptimaComparar[0]))
+                    if plantacionesRepetidas == len(secuenciaOptimaComparar[0]):
+                        #Todo estÃ¡ malo, no tocar secuencia optima
+                        nuevaSecuenciaInsertar = copy.deepcopy(secuenciaSaltar)
+                        #print("nuevaSecuenciaInsertar: ",nuevaSecuenciaInsertar, "Secuencia optima: ", secuenciaOptima)
+                        #nuevaSecuenciaInsertar[0].append(nuevaPlantacion[0][0])
+                        #nuevaSecuenciaInsertar[1] += int(nuevaPlantacion[1])
+                        #nuevaSecuenciaInsertar[2] += costoRiego(nuevaPlantacion[0][0], int(secuenciaOptima[1]))
+                        #print("nuevaSecuenciaInsertarPOST: ",nuevaSecuenciaInsertar)
+                        #print("Evaluando mejor entre ",nuevaSecuenciaInsertar," y ",secuenciaOptimaComparar)
+                        if nuevaSecuenciaInsertar[2] < secuenciaOptimaComparar[2]:
+                            #secuenciaOptimaComparar = copy.deepcopy(nuevaSecuenciaInsertar)
+                            #print("Secuencias: ", secuencias[indiceSecuencias+1])
+                            for secuencia in secuencias[indiceSecuencias+1]:
+                                #print("Comparando ",secuencia," con ",secuenciaOptimaComparar)
+                                if str(secuencia) == str(secuenciaOptimaComparar):
+                                    #print("iguales!! INSERTANDO ",nuevaSecuenciaInsertar," EN LUGAR DE ", secuencia)
+                                    secuencias[indiceSecuencias+1][secuencias[indiceSecuencias+1].index(secuencia)] = copy.deepcopy(nuevaSecuenciaInsertar)
+                                    #print("Secuencias: ", secuencias[indiceSecuencias+1])
+                                    break
+                            #secuencias[indiceSecuencias+1][secuencias.index(secuenciaOptimaComparar)] = copy.deepcopy(nuevaSecuenciaInsertar)
+                            #print("nueva Secuencia optima comparar: ",secuenciaOptimaComparar)
+                            #print("Secuencias: ", secuencias[indiceSecuencias+1])
+                        seEncontro = True
+                        break
+                if seEncontro == True:
+                    continue
+
                 #Marica rece unos 20 padre nuestros para entender este puto condicional oyo?
-                #Basicamente, analiza si es mejor regar secuenciaOptima primero o regar nuevaPlantacion primero, esto lo hace sumando el costo de regar alguno de primero y el costo de regar el otro después, agarra el menor.
-                print("nueva plantacion[0][0]: ",nuevaPlantacion[0][0])
+                #Basicamente, analiza si es mejor regar secuenciaOptima primero o regar nuevaPlantacion primero, esto lo hace sumando el costo de regar alguno de primero y el costo de regar el otro despuï¿½s, agarra el menor.
+                #print("nueva plantacion[0][0]: ",nuevaPlantacion[0][0])
                 if indiceSecuencias == 0:
-                    print("Secuencia optima: ", secuenciaOptima)
+                    #print("Secuencia optima: ", secuenciaOptima)
                     if secuenciaOptima[2]+costoRiego(nuevaPlantacion[0][0], secuenciaOptima[1]) < nuevaPlantacion[2]+costoRiego(secuenciaOptima[0][0], nuevaPlantacion[1]):
                         #secuencias[indiceSecuencias+1].append([[secuenciaOptima[0].append(nuevaPlantacion[0])], secuenciaOptima[1]+nuevaPlantacion[0].tiempoRiego, secuenciaOptima[2]+costoRiego(nuevaPlantacion[0], secuenciaOptima[1])])
                         secuencias[indiceSecuencias+1].append([[secuenciaOptima[0][0], nuevaPlantacion[0][0]],int(secuenciaOptima[1])+int(nuevaPlantacion[0][0].tiempoRiego), secuenciaOptima[2]+costoRiego(nuevaPlantacion[0][0], secuenciaOptima[1])])
@@ -97,21 +162,23 @@ def ordenOptimoDinamica(finca):
                         secuencias[indiceSecuencias+1].append([[nuevaPlantacion[0][0], secuenciaOptima[0][0]],int(secuenciaOptima[1])+int(nuevaPlantacion[0][0].tiempoRiego), nuevaPlantacion[2]+costoRiego(secuenciaOptima[0][0], nuevaPlantacion[0][0].tiempoRiego)])
                         #print("secuencias[",indiceSecuencias+1,"]: ", secuencias[indiceSecuencias+1])
                 else:
-                    secuenciaOptima[0].append(nuevaPlantacion[0][0])
-                    secuencias[indiceSecuencias+1].append([secuenciaOptima[0],int(secuenciaOptima[1])+int(nuevaPlantacion[0][0].tiempoRiego), secuenciaOptima[2]+costoRiego(nuevaPlantacion[0][0], secuenciaOptima[1])])
-                    print("secuencias[",indiceSecuencias+1,"]: ", secuencias[indiceSecuencias+1])
-                    break
+                    #secuenciaOptima[0].append(nuevaPlantacion[0][0])
+                    secuenciaNueva = copy.copy(secuenciaOptima[0])
+                    secuenciaNueva.append(nuevaPlantacion[0][0])
+                    secuencias[indiceSecuencias+1].append([secuenciaNueva,int(secuenciaOptima[1])+int(nuevaPlantacion[0][0].tiempoRiego), secuenciaOptima[2]+costoRiego(nuevaPlantacion[0][0], secuenciaOptima[1])])
+                    #print("secuencias[",indiceSecuencias+1,"]: ", secuencias[indiceSecuencias+1])
+                    #break
 
             #longitudBackup.remove(secuenciaOptima)
         indiceSecuencias+=1
-        print("Indice secuencias: ",indiceSecuencias)
-        if indiceSecuencias > 3:
-            break
+        #print("Indice secuencias: ",indiceSecuencias)
+        #if indiceSecuencias > 3:
+        #    break
     return secuencias
 
 
 
-finca = formatInput('BateriaPruebas/Prueba1.txt')
+finca = formatInput('BateriaPruebas/Prueba11.txt')
 ordenOptimo = ordenOptimoDinamica(finca)
 print("Orden optimo:")
 tamano = 1
@@ -119,10 +186,10 @@ menor = float('inf')
 for longitud in ordenOptimo:
     
     for secuenciaOptima in longitud:
-        if tamano == 5 and (secuenciaOptima[2]<menor):
-            menor = secuenciaOptima[2]
-            print("Secuencia de tamano ",tamano,": ",secuenciaOptima , "\n")
+        #if tamano == 5 and (secuenciaOptima[2]<menor):
+        #    menor = secuenciaOptima[2]
+        print("Secuencia de tamano ",tamano,": ",secuenciaOptima , "\n")
     tamano += 1
 
-#El dia de hoy me acostaré, con dos personas sabiendo cómo funciona este código, estas personas siendo yo y dios.
-#Espero al levantarme mañana, que sólo dios recuerde.
+#El dia de hoy me acostarÃ©, con dos personas sabiendo cÃ³mo funciona este cÃ³digo, estas personas siendo yo y dios.
+#Espero al levantarme maÃ±ana, que sÃ³lo dios recuerde.
